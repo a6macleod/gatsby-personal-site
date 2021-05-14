b0fc4ea9ae23c6b9bd7201adf051c784712c32a6
Andrew MacLeod <A6macleod@gmail.com>
2021-05-13 21:31:21 -0400

    Update gatsby-source-filesystem path

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/data/`,
+        path: `${__dirname}/src/data/`,
         typeName: `Json`, // a fixed string
       },
     },
